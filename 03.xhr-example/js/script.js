c1130469e461a9b0cf307bce51339b9c5d2b7e1e
Jawad-Nassiri <jawadnasiri2222@gmail.com>
2025-03-06 13:23:54 +0100

    parse the data and show them

--- 03.xhr-example/js/script.js
+++ 03.xhr-example/js/script.js
@@ -19,20 +19,21 @@
 
 
 
+// the second and the old method to do //! not recommended
 
 
+// const btnElement = document.querySelector('button')
+// const outputElement = document.querySelector('.output')
 
-const btnElement = document.querySelector('button')
-const outputElement = document.querySelector('.output')
+// btnElement.addEventListener('click', () => {
+//     let xhr = new XMLHttpRequest()
+//     xhr.open('GET', './text.txt')
 
-btnElement.onclick = () => {
-    let xhr = new XMLHttpRequest()
-    xhr.open('GET', './text.txt')
+//     xhr.onreadystatechange = () => {
+//         if(xhr.status === 200 && xhr.readyState === 4) {
+//             outputElement.textContent = xhr.responseText
+//         }
+//     }
 
-    xhr.onload = () => {
-        if(xhr.status === 200) {
-            outputElement.textContent = xhr.responseText;
-        }
-    }
-
-}
+//     xhr.send()
+// })
